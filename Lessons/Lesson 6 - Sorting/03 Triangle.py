f656455ba724bcb1d0007f507f6847fee0a0e2e8
Sarel <sarelsch@gmail.com>
2022-09-20 16:38:44 +0300

    Lesson 6 - sorting - Triangle

---
+++ Lessons/Lesson 6 - Sorting/03 Triangle.py
@@ -0,0 +1,9 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A):
+    A = sorted(A)
+    for i in range(len(A)-2):
+        if (A[i] + A[i+1] > A[i+2]):
+            return 1
+    return 0
